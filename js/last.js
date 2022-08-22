b7f5bfe3ce6fd19d7f4b5a4aac9ca292bdd3b9cc
ALLCOINLAB <all.coin.lab@gmail.com>
2022-08-22 13:13:03 +0900

    a

--- js/last.js
+++ js/last.js
@@ -1,3 +1,6 @@
 $('#modal').on('shown.bs.modal', function () {
   $('#modal').trigger('focus')
 })
+$('#modal-select-output').on('shown.bs.modal', function () {
+  $('#modal-select-output').trigger('focus')
+})
