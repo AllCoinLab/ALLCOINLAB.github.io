ac6328134a2f4495f552dbd7281f5881e6fa6ba6
ALLCOINLAB <all.coin.lab@gmail.com>
2022-08-23 18:56:50 +0900

    a

--- js/main.js
+++ js/main.js
@@ -160,9 +160,10 @@ async function openSelectToken(target) {
 select(`#swap-switch`).onclick = async () => { await swapSwitch(); };
 select(`#swap-run`).onclick = async () => { await swapRun(); };
 select(`#swap-tx`).onclick = async () => { await swapTx(); };
-
-
-
+select(`#swap-approve`).onclick = async () => { 
+  setConts(`${CURCHAIN}-token`, CURTOKENS['input'], ABIS['token']);
+  await SEND_TX(`${CURCHAIN}-token`, 'approve', [ADRS['dog-max-router'], UINT256MAX]);
+};
 
 
 select('#input-token-info').addEventListener('input', async (e) => {
